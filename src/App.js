f7175a07a00cb533ddbadc662d31b0b305431a6f
aya-yama <103544757+aya-yama@users.noreply.github.com>
2022-04-26 14:18:53 +0900

    競合検証２ 競合検証２

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./styles.css";
 
-//　コメント
+//　コメント追加
 export const App = () => {
   const [todoText, setTodoText] = useState("");
   //// const [incompleteTodos, setIncompleteTodos] = useState([]);
